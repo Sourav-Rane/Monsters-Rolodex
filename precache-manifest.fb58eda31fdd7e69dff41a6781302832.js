878763274e49623bca7039149449722636aca7a6
Sourav Rane <songokuonfire@gmail.com>
2020-04-03 12:22:54 +0530

    Updates

---
+++ precache-manifest.fb58eda31fdd7e69dff41a6781302832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e98410873bfe865fa800d6e601d9959",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "6752820366bf65eb7af2",
+    "url": "/Monsters-Rolodex/static/css/main.1d0124f7.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/Monsters-Rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6752820366bf65eb7af2",
+    "url": "/Monsters-Rolodex/static/js/main.6f047051.chunk.js"
+  },
+  {
+    "revision": "d92c59ecab1be31e7a67",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.a47f78f7.js"
+  }
+]);
